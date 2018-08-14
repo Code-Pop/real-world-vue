a0072c6743de1cd73b0f063ff3fe916ab69970e0
Adam Jahr <adamjahr@gmail.com>
2018-08-14 18:42:36 -0400

    Lesson 7 Ending Code

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -3,7 +3,7 @@
     <div class="event-card -shadow">
       <span class="eyebrow">@{{ event.time }} on {{ event.date }}</span>
       <h4 class="title">{{ event.title }}</h4>
-      <span>{{ event.attendees.length }} attending</span>
+      <BaseIcon name="users">{{ event.attendees.length }} attending</BaseIcon>
     </div>
   </router-link>
 </template>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import BaseIcon from '@/components/BaseIcon'
+
+Vue.component('BaseIcon', BaseIcon)
 
 Vue.config.productionTip = false
 
