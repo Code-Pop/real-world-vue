78794499eada80334824d29be575031e997bfad2
Gregg Pollack <greggpollack+github@gmail.com>
2018-08-16 10:52:53 -0400

    Now using Axios to get event listing

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <router-link class="event-link" :to="{ name: 'event-show', params: { id: '1' } }">
+  <router-link class="event-link" :to="{ name: 'event-show', params: { id: event.id } }">
     <div class="event-card -shadow">
       <span class="eyebrow">@{{ event.time }} on {{ event.date }}</span>
       <h4 class="title">{{ event.title }}</h4>
@@ -10,19 +10,8 @@
 
 <script>
 export default {
-  data() {
-    return {
-      event: {
-        id: 1,
-        title: 'Beach Cleanup',
-        date: 'Tues Aug 19, 2018',
-        time: '6:00',
-        attendees: [
-          { id: 'abc123', name: 'Adam Jahr' },
-          { id: 'def456', name: 'Gregg Pollack' }
-        ]
-      }
-    }
+  props: {
+    event: Object
   }
 }
 </script>
--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,16 +1,32 @@
 <template>
   <div>
     <h1>Events Listing</h1>
-    <EventCard/>
+    <EventCard v-for="event in events" :key="event.id" :event="event"/>
   </div>
 </template>
 
 <script>
 import EventCard from '@/components/EventCard.vue'
+import axios from 'axios'
 
 export default {
   components: {
     EventCard
+  },
+  data() {
+    return {
+      events: []
+    }
+  },
+  mounted() {
+    axios
+      .get('http://localhost:3000/events')
+      .then(response => {
+        this.events = response.data
+      })
+      .catch(error => {
+        console.log('There was an error:', error.response)
+      })
   }
 }
 </script>
