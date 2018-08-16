57820ed1c5727ec369469136fff359a230e4573d
Gregg Pollack <greggpollack+github@gmail.com>
2018-08-16 11:21:09 -0400

    Now a little more organized, with an EventService

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -7,7 +7,7 @@
 
 <script>
 import EventCard from '@/components/EventCard.vue'
-import axios from 'axios'
+import EventService from '@/services/EventService.js'
 
 export default {
   components: {
@@ -19,8 +19,7 @@ export default {
     }
   },
   mounted() {
-    axios
-      .get('http://localhost:3000/events')
+    EventService.getEvents()
       .then(response => {
         this.events = response.data
       })
