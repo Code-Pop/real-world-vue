90e71252641817807178fe1c4e2ae4835e9b9d0e
Gregg Pollack <greggpollack+github@gmail.com>
2018-08-30 14:58:32 -0400

    Added slot to BaseIcon

--- src/components/BaseIcon.vue
+++ src/components/BaseIcon.vue
@@ -3,6 +3,7 @@
       <svg class='icon' :width="width" :height="height">
         <use v-bind="{'xlink:href':'/feather-sprite.svg#' + name}"/>
       </svg>
+      <slot></slot>
     </div>
 </template>
     
