1a5e9feb711c0d60c3de8b35bb58d5a817f1ef9c
Adam Jahr <adamjahr@gmail.com>
2018-09-18 11:15:22 -0400

    lesson11-state&getters-finish

--- src/router.js
+++ src/router.js
@@ -15,15 +15,15 @@ export default new Router({
       component: EventList
     },
     {
+      path: '/event/create',
+      name: 'event-create',
+      component: EventCreate
+    },
+    {
       path: '/event/:id',
       name: 'event-show',
       component: EventShow,
       props: true
-    },
-    {
-      path: '/event/create',
-      name: 'event-create',
-      component: EventCreate
     }
   ]
 })
--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,3 +1,21 @@
 <template>
-  <h1>Create Event</h1>
+  <div>
+    <h1>Create an Event, {{ user.name }}</h1>
+    <p>This event was created by {{ user.name }}</p>
+    <p>{{ getEventById(2) }}</p>
+  </div>
 </template>
+
+<script>
+import { mapState, mapGetters } from 'vuex'
+
+export default {
+  computed: {
+    ...mapGetters(['getEventById']),
+    ...mapState(['user', 'categories'])
+  }
+}
+</script>
+
+<style scoped>
+</style>
