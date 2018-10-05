6d25969adc81d3d14b513ff2b6f109ada157439e
Gregg Pollack <greggpollack+github@gmail.com>
2018-10-05 17:08:16 -0400

    User name module

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import upperFirst from 'lodash/upperFirst'
 import camelCase from 'lodash/camelCase'
 import App from './App.vue'
 import router from './router'
-import store from './store'
+import store from './store/store'
 import BaseIcon from '@/components/BaseIcon'
 
 Vue.component('BaseIcon', BaseIcon)
---
+++ src/store/modules/user.js
@@ -0,0 +1,6 @@
+export default {
+  state: {
+    id: 'abc123',
+    name: 'Adam'
+  }
+}
