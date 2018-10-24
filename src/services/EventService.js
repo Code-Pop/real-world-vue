73ef02e7059d69f6ef63e05c7e27ce9ed511e317
Gregg Pollack <greggpollack+github@gmail.com>
2018-10-24 16:25:03 -0400

    Progress bar with axios interceptors

--- src/services/EventService.js
+++ src/services/EventService.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import NProgress from 'nprogress'
 
 const apiClient = axios.create({
   baseURL: `http://localhost:3000`,
@@ -7,6 +8,16 @@ const apiClient = axios.create({
     Accept: 'application/json',
     'Content-Type': 'application/json'
   }
+})
+
+apiClient.interceptors.request.use(config => {
+  NProgress.start()
+  return config
+})
+
+apiClient.interceptors.response.use(response => {
+  NProgress.done()
+  return response
 })
 
 export default {
