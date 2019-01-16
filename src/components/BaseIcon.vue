0c5016f342ed9e68d8408c4d7621f766ac9f2156
Adam Jahr <adamjahr@gmail.com>
2019-01-16 13:42:03 -0500

    updated Vuelidate 2 code

--- src/components/BaseIcon.vue
+++ src/components/BaseIcon.vue
@@ -1,32 +1,43 @@
 <template>
-  <div>
-    <label v-if="label">{{ label }}</label>
-    <input :value="value" @input="updateValue" v-on="listeners" v-bind="$attrs">
+  <div class="icon-wrapper">
+    <svg class="icon" :width="width" :height="height">
+      <use v-bind="{'xlink:href':'/feather-sprite.svg#' + name}"></use>
+    </svg>
+    <slot></slot>
   </div>
 </template>
-
+    
 <script>
 export default {
-  inheritAttrs: false,
+  name: 'Icon',
   props: {
-    label: {
-      type: String,
-      default: ''
+    name: String,
+    width: {
+      type: [Number, String],
+      default: 24
     },
-    value: [String, Number]
-  },
-  computed: {
-    listeners() {
-      return {
-        ...this.$listeners,
-        input: this.updateValue
-      }
-    }
-  },
-  methods: {
-    updateValue(event) {
-      this.$emit('input', event.target.value)
+    height: {
+      type: [Number, String],
+      default: 24
     }
   }
 }
 </script>
+    
+<style scoped>
+.icon-wrapper {
+  display: inline-flex;
+  align-items: center;
+  color: rgba(0, 0, 0, 0.4);
+  font-size: 1rem;
+  font-weight: 600;
+}
+.icon {
+  stroke: currentColor;
+  stroke-width: 2;
+  stroke-linecap: round;
+  stroke-linejoin: round;
+  fill: none;
+  margin-right: 6px;
+}
+</style>
